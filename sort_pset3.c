3645b6e93a8130f5d73f1300201564733e4a89a6
andrayantelo <andrayantelo@gmail.com>
2017-02-08 15:40:35 -0800

    figured out how to implement sort

--- sort_pset3.c
+++ sort_pset3.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-#include <cs50.h>
+//#include <cs50.h>
 
 void sort(int values[], int n);
 int main(void) {
     int values[5];
-    values[0] = 2;
-    values[1] = 1;
-    values[2] = 10;
-    values[3] = 0;
-    values[4] = 5;
+    values[0] = 5;
+    values[1] = 4;
+    values[2] = 3;
+    values[3] = 2;
+    values[4] = 1;
+    
+    printf("values before sorting: \n");
+    int y;
+    for (y = 0; y < 5; y++) {
+        printf("values: %d\n", values[y]);
+    }
+    
     sort(values, 5);
+    printf("values after sorting: \n");
+    for (y = 0; y < 5; y++) {
+        printf("values: %d\n", values[y]);
+    }
 }
 
 
@@ -18,41 +29,36 @@ void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
     // Let's try selection sort.
-    
-    // Find smallest unsosrted element
+   
     int i;
     int j;
-    //int smallest_element;
-    int k;
-    int values_copy[n];
-    for (k = 0; k < n; k++) {
-        values_copy[k] = values[k];
-    }
     int place_counter = 0;
+    
     for (i = 0; i < n; i++) {
         //Take ith element and compare it to all the other elements
-        printf("currently checking value: %d\n", values[i]);
-        for (j = 0; j < n; j++) {
-            if (values_copy[i] > values_copy[j]) {
+        printf("checking value: %d\n", values[i]);
+        for (j = place_counter; j < n; j++) {
+            if (values[i] > values[j]) {
                 //break out of loop
                 j = n;
             }
             
         }
         if (j == n) {
-            printf("smallest element: %d\n", values_copy[i]);
             // Swap places between the smallest element and the element currently in the first position
-            values[i] = values[place_counter];
-            values[place_counter] = values_copy[i];
+            int smallest_element = values[i];
+            int swap_element = values[place_counter];
+
+            values[i] = swap_element;
+            values[place_counter] = smallest_element;
+            
+            // have to move to the next value in values.. the value after the recently sorted smallest element.
+            i = place_counter;
+            
             place_counter += 1;
-            // have to move to the next value in values.. i mean after the recently sorted smallest element.
-            //i = place_counter;
+            
+
         }
     }
-    int z;
-    for(z=0;z<n;z++) {
-        printf("values: %d\n", values[z]); 
-    }
-
-   
 }
+
