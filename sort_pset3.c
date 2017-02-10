4ccd55d92b4814e78bf960e51b365ec8eeb96ecc
andrayantelo <andrayantelo@gmail.com>
2017-02-10 10:14:30 -0800

    cleaned up the code for the search function

--- sort_pset3.c
+++ sort_pset3.c
@@ -6,26 +6,24 @@ void sort(int values[], int n);
 bool search(int value, int values[], int n);
 
 int main(void) {
-    int values[3];
-    values[0] = 40;
-    values[1] = 41;
-    values[2] = 42;
-
-
-    
+    int values[4];
+    values[0] = 39;
+    values[1] = 40;
+    values[2] = 41;
+    values[3] = 42;
     //printf("values before sorting: \n");
     //int y;
     //for (y = 0; y < 5; y++) {
     //    printf("values: %d\n", values[y]);
     //}
     
-    sort(values, 3);
+    sort(values, 4);
     //printf("values after sorting: \n");
     //for (y = 0; y < 5; y++) {
     //    printf("values: %d\n", values[y]);
     //}
     
-    bool found = search(42, values, 3);
+    bool found = search(42, values, 4);
     if (found == true) {
         printf("Found the value\n");
     }
@@ -74,11 +72,6 @@ void sort(int values[], int n)
 
 bool search(int value, int values[], int n)
 {
-    printf("looking through: \n");
-    int x;
-    for (x = 0; x < n; x++) {
-        printf("values[%d] : %d\n", x, values[x]);
-    }
     // TODO: implement a searching algorithm
     // Return false if n is non-positive
     if (n < 0) {
@@ -102,47 +95,36 @@ bool search(int value, int values[], int n)
     if (value == midpoint) {
         return 1;
     }
-    printf("the midpoint is %d\n", midpoint);
+    
     int i;
     int j;
     // Check if value is to the left of midpoint
     if (value < midpoint) {
-        printf("value is in the left half\n");
+      
         // if it is, then throw away right half, get the left half, and
         // search through it
 
         // get left half
        
-        int values_copy[n/2];
-        int counter = 0;
+        int values_copy[n/2 - 1];
+       
         for (i = 0; i < n/2; i++) {
-            counter += 1;
             values_copy[i] = values[i];
         };
-        printf("counter: %d\n", counter);
-        search(value, values_copy, n/2 );
+        
+        return search(value, values_copy, n/2 );
     }
-    else if (value > values[n/2]) {
-        printf("value is in the right half\n");
-        //get right half
-        printf("size of values_copy: %d\n", n/2);
-        int values_copy[n/2 -1];
-        int counter = 0;
+    else if (value > midpoint) {
+        // Get the right half.
+        int values_copy[n/2];
+
         for (j = 0; j <  n/2; j++) {
-            counter += 1;
             values_copy[j] = values[n/2  + 1 + j];
         };
-        printf("counter: %d\n", counter);
-        
-        // print out values_copy to see
-        int z;
-        for (z=0;z<n/2;z++) {
-            printf("values_copy[%d]: %d \n", z, values_copy[z]);
-        }
-        search(value, values_copy, n/2 - 1);
+      
+        // Search through the right half.
+        return search(value, values_copy, n/2 - 1);
     }
 
+    return false;
 }
-    
-
-
