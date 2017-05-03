ef9526fcdd0104800a27acff6d38195770877148
Andrea Anaya <andrayantelo@gmail.com>
2017-05-03 15:16:21 -0700

    added image file

--- recover/recover.c
+++ recover/recover.c
@@ -3,15 +3,21 @@
 int main(int argc, char *argv[]) {
     
     // Ensure only one command line argument
-    if (argc != 1) {
-        fprintf(stderr, "Usage: ./recover infile\n");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./recover image\n");
         return 1;
     }
     
+    // Remember file names
+    char *infile = argv[1];
+    
+    // Open input file
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
+    
+    printf("size of image file: %zu\n", sizeof(infile));
 }
